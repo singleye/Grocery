e8bfe040d1f80f90c57e51245c510d951b10bcbc
singleye <singleye@163.com>
2018-02-26 17:26:33 +0800

    Add 'district' model

---
+++ ChinaDistrict/district/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class District(models.Model):
+    code = models.CharField(max_length=16, primary_key=True, unique=True, null=False, blank=False)
+    name = models.CharField(max_length=256, null=False, blank=False)
+    parent_code = models.CharField(max_length=16, null=True, blank=False)
+    level = models.IntegerField(null=False, blank=False)
+
+    class Meta:
+        db_table = "district"
